0ce1c776ffe30cb8a1c97bea0431bbb9675278bf
jbrunops <jbrunops@outlook.com>
2024-12-24 14:11:00 -0300

    Parâmetros duplicados

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ console.log(student.point);
 
 // Tentando deletar uma propriedade de um objeto que não posso deletar.
 // delete window.document;
+
+// Quando passamos parâmetros duplicados
+function sum(a, a, c) {
+  return a + b + c;
+}
+
+const result = sum(1, 3, 2);
+console.log(result);
+// O resultado é oito (pegadinha) 3 + 3 = 6 + 2.

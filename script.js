0c3e03ea9c866aa35afd77a257957b403a7f64d3
jbrunops <jbrunops@outlook.com>
2024-12-24 13:56:02 -0300

    Estudo - Strict Mode

---
+++ script.js
@@ -0,0 +1,9 @@
+// Strict mode (modo restrito): ativando esse modo, os erros que eram solenciosos passam a gerar exceções no JavaScript
+
+function showMessage() {
+  personName = "Jackson Bruno";
+
+  console.log("Olá", personName);
+}
+
+showMessage();

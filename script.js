a27657d3d613d1f25e9d056388c898f07bc19cb8
jbrunops <jbrunops@outlook.com>
2024-12-24 14:02:00 -0300

    Escopo global strict mode

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Strict mode (modo restrito): ativando esse modo, os erros que eram solenciosos passam a gerar exceções no JavaScript
 
 function showMessage() {
+  "use strict";
   personName = "Jackson Bruno";
 
   console.log("Olá", personName);
